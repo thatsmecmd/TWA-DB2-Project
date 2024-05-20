90ff45f9329b71ec58667c12134d39ba1ff70e0f
jay <LE2231057@crc-lennox.qc.ca>
2024-05-20 12:22:21 -0400

    edited server to make it work for deployment

--- server/controllers/graph.controller.js
+++ server/controllers/graph.controller.js
@@ -5,14 +5,19 @@ const {spawn} = require('child_process');
 // run the python script and return the std output, if there are no errors.
 async function get_fossil_fuel_image(country) {
     // executing the python script
-    const python = await spawn('python', ['./Python_Scripts/graph1.py', country]); // execute the python script
+    const python = await spawn('python3', ['./Python_Scripts/graph1.py', country]); // execute the python script
     
     let std = ''; // the script's STD output
+    let stderr = ''; // the scipt's error output, if there is any.
 
     // gets the scripts logs
     python.stdout.on('data', function(data) {
         console.log(`Python script output: ${data}`);
         std += data.toString();
+    });
+
+    python.stderr.on('data', (data) => {
+        stderr += data.toString();
     });
 
     // return a promise of the python logs, or an error if the python code can't execute properly.
@@ -21,7 +26,7 @@ async function get_fossil_fuel_image(country) {
             if (code === 0) {
                 resolve(std);
             } else {
-                reject('Error while running python code. console output: ' + std);
+                reject('Error while running python code. console output: ' + stderr);
             }
         });
     });
@@ -31,14 +36,19 @@ async function get_sustainable_energy_image(year, countries){
     // creating the argument array 
     args = ['./Python_Scripts/graph2.py', year].concat(countries);
     // executing the python script
-    const python = await spawn('python', args); // execute the python script
+    const python = await spawn('python3', args); // execute the python script
     
     let std = ''; // the script's STD output
+    let stderr = ''; // the script's error output, if there are any
 
     // gets the scripts logs
     python.stdout.on('data', function(data) {
         console.log(`Python script output: ${data}`);
         std += data.toString();
+    });
+
+    python.stderr.on('data', (data) => {
+        stderr += data.toString();
     });
 
     // return a promise of the python logs, or an error if the python code can't execute properly.
@@ -47,16 +57,17 @@ async function get_sustainable_energy_image(year, countries){
             if (code === 0) {
                 resolve(std);
             } else {
-                reject('Error while running python code. console output: ' + std);
+                reject('Error while running python code. console output: ' + stderr);
             }
         });
     });
 }
 
 async function run_graph3(param, end_year){
-    const python = await spawn('python', ['./Python_Scripts/graph3.py', param, end_year]); // execute the python script
+    const python = await spawn('python3', ['./Python_Scripts/graph3.py', param, end_year]); // execute the python script
 
     let std = ''; // the script's STD output
+    let stderr = ''; // the script's stderr output
 
     // gets the scripts logs
     python.stdout.on('data', function(data) {
@@ -65,13 +76,17 @@ async function run_graph3(param, end_year){
         console.log("qytr");
     });
 
+    python.stderr.on('data', (data) => {
+        stderr += data.toString();
+    });
+
     // return a promise of the python logs, or an error if the python code can't execute properly.
     return new Promise((resolve, reject) => {
         python.on('close', (code) => {
             if (code === 0) {
                 resolve(std);
             } else {
-                reject('Error while running python code. console output: ' + std);
+                reject('Error while running python code. console output: ' + stderr);
             }
         });
     });
@@ -79,9 +94,10 @@ async function run_graph3(param, end_year){
 }
 
 async function run_graph4(country){
-    const python = await spawn('python', ['./Python_Scripts/graph4.py', country]);
+    const python = await spawn('python3', ['./Python_Scripts/graph4.py', country]);
 
     let std = ''; // the script's STD output
+    let stderr = ''; // the script's stderr output
 
     // gets the scripts logs
     python.stdout.on('data', function(data) {
@@ -90,13 +106,17 @@ async function run_graph4(country){
         console.log("qytr");
     });
 
+    python.stderr.on('data', (data) => {
+        stderr += data.toString();
+    });
+
     // return a promise of the python logs, or an error if the python code can't execute properly.
     return new Promise((resolve, reject) => {
         python.on('close', (code) => {
             if (code === 0) {
                 resolve(std);
             } else {
-                reject('Error while running python code. console output: ' + std);
+                reject('Error while running python code. console output: ' + stderr);
             }
         });
     });
